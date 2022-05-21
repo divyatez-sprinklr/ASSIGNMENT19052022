d0ed22c7ec709dc46680012c7f32a23c3185c3df
d3ad5h01 <iit2019220@iiita.ac.in>
2022-05-21 15:48:38 +0530

    Updated textfield

--- app.js
+++ app.js
@@ -1,31 +1,24 @@
 import data from "./data.js";
 
-var selectedIndex,selectedItem,selectedItemLink;
+var selectedIndex;
 
 const element = document.createElement("img");
-const editNameInput = document.createElement("input");
-const viewImageName = document.createElement("p"); 
+const editNameInput = document.createElement("textarea");
 const mainContainer = document.querySelector(".selection-container"); 
-
+const viewContainer = document.querySelector(".view-container");
 const editContainer = document.getElementById('edit-container');
-var toggle;
 
 function cropTitle(name){
     if(name.length>30)
     {
         var tmp = name.substr(0,15) +"..."+name.substr(name.length-15,15);
-
         return tmp;
     }
     else
         return name;
 }
 
-function editImg(newSelected){
-    selectedIndex = newSelected;
-    element.setAttribute("src", data[selectedIndex].previewImage);
-    viewImageName.innerText = data[selectedIndex].title;
-}
+
 
 
 function createElementHTML(title,previewImage)
@@ -38,28 +31,31 @@ function createElementHTML(title,previewImage)
 </div>`;
 }
 
-function createElementHTML2(title,previewImage)
-{
-    return `div class="selection-item-element">
-    <img class="selection-item-element-image" src="${previewImage}" alt="" >
-</div>
-<div class="selection-item-element">
-    <p>${cropTitle(title)}</p>
-</div>`;
+function handleViewImage(newIndex){
+    element.setAttribute("src", data[newIndex].previewImage);
+    editNameInput.value = data[newIndex].title;
+    editNameInput.style.width = `${data[newIndex].title.length}ch`;
+    
 }
 
-
-function setSelected(index, item , link)
+function handleActiveItem(previousIndex,newIndex)
 {
-    [selectedIndex,selectedItem,selectedItemLink] = [index, item, link];
+    document.getElementById(`selectedItem${previousIndex}`).style.backgroundColor = 'transparent';
+    document.getElementById(`selectedItem${previousIndex}`).style.color = 'black';
+    document.getElementById(`selectedItem${newIndex}`).style.backgroundColor = '#0453c8';
+    document.getElementById(`selectedItem${newIndex}`).style.color = 'white';
+    element.setAttribute("src", data[newIndex].previewImage);
 }
+
 
 function toggleSelected(index){
-    document.getElementById(`selectedItem${selectedIndex}`).style.backgroundColor = 'transparent';
-    editImg(index)
-    document.getElementById(`selectedItem${selectedIndex}`).style.backgroundColor = '#0453c8';
+
+    handleActiveItem(selectedIndex,index);
+    handleViewImage(index);
+    selectedIndex = index;
     console.log("Clicked"+index);
 }
+
 
 function addInitalElements(){
     const selectionItem = document.getElementById("selection-container");
@@ -68,97 +64,64 @@ function addInitalElements(){
     data.map((item, index) => {
         
         let newItem = document.createElement("div");
-        if(index==0)
-        {
-            setSelected(index,newItem,item.previewImage);   
-        }
-
+    
         newItem.classList.add('selection-item');
         newItem.innerHTML = createElementHTML(item.title,item.previewImage);
         newItem.setAttribute("id",`selectedItem${index}`);
         newItem.onclick = () => toggleSelected(index);
         selectionItem.appendChild(newItem);
-  });
+    });
+
+    editNameInput.style.display="block";
+    editNameInput.setAttribute("type","text");
+    editNameInput.oninput=(event) =>{'this.style.height = "";this.style.height = this.scrollHeight + "px"'};
+    editNameInput.style.overflow = `hidden`;
+    editNameInput.style.resize = `none`;
+    
+
+    editNameInput.onkeypress = (event)=> {
+        if(event.keyCode==13){handleEditName(selectedIndex);}
+    };
+    viewContainer.prepend(element);
+    viewContainer.append(editNameInput);
 }
 
 document.addEventListener("keydown", function(event) {
     if(event.keyCode==38){
         console.log("Up key");
-        if(selectedIndex==0)
-        {
-            toggleSelected(data.length-1);
-        }
-        else
-            toggleSelected(selectedIndex-1);
+        if(selectedIndex==0) toggleSelected(data.length-1);
+        else toggleSelected(selectedIndex-1);
     }
     else if(event.keyCode==40){
-        if(selectedIndex==data.length-1)
-        {
-            toggleSelected(0);
-        }
-        else
-            toggleSelected(selectedIndex+1);
+        if(selectedIndex==data.length-1) toggleSelected(0);
+        else toggleSelected(selectedIndex+1);
     }   
 });
 
 
- function handleEditName()
+ function handleEditName(index)
  {
     
-    var editedItem = document.getElementById(`selectedItem${selectedIndex}`);
-    console.log(editedItem);
+    var editedItem = document.getElementById(`selectedItem${index}`);
     
-    data[selectedIndex].title = editNameInput.value;
-    viewImageName.innerText = data[selectedIndex].title;
-    editedItem.innerHTML = `<div class="selection-item-element">
-    <img class="selection-item-element-image" src="${data[selectedIndex].previewImage}" alt="" >
-</div>
-<div class="selection-item-element">
-    <p>${cropTitle(data[selectedIndex].title)}</p>
-</div>`;
-    editContainer.removeChild(editContainer.children[1]);
-    editContainer.prepend(viewImageName);
-    //editContainer.innerHTML = viewImageName;
-    //console.log("Edit closed"); 
-    toggle = false;
+    data[index].title = editNameInput.value;
+    editedItem.innerHTML = createElementHTML(data[index].title,data[index].previewImage);
+
+    document.activeElement.blur();
  }
 
-function handleViewName(){
-    editContainer.removeChild(editContainer.children[1]);
-    editContainer.prepend(editNameInput);
-    console.log(editNameInput);
-    editNameInput.value = data[selectedIndex].title;
-    toggle = true;
+
+function resizeInput() {
+    this.style.width = this.value.length + "ch";
+    this.style.height = "auto";
+    this.style.height = (this.scrollHeight) + "px";
 }
-
+  
+editNameInput.addEventListener('input', resizeInput); 
+ 
 function main(){
-    addInitalElements();
-
     selectedIndex=0;
-    toggle = false;
-    document.getElementById(`selectedItem${selectedIndex}`).style.backgroundColor = '#0453c8';
-    const viewContainer = document.querySelector(".view-container");
-    element.setAttribute("src", data[0].previewImage);
-    viewImageName.style.display="block";
-    editNameInput.style.display="block";
-    console.log(editContainer);
-    
-    editContainer.innerHTML = `<p>${data[0].title}</p>`;
-    
-    //viewImageName.innerText = data[0].title;
-    //editNameInput.value = data[0].title;
-
-    editContainer.onclick = ()=> handleViewName();
-
-    editContainer.onkeypress = (event)=> {
-        if(event.keyCode==13&&toggle){handleEditName();}
-    };
-    
-   // editNameInput.style.display="none";
-    
-    viewContainer.prepend(element);
-    // viewContainer.append(viewImageName);
-    // viewContainer.append(editNameInput);
-
+    addInitalElements();
+    toggleSelected(0);
 }
 main();
